ccb8b0cb40acb625a701ccb9d3e1f97e3df4783c
paulkleinschmidt <paul.kleinschmidt1@gmail.com>
2018-01-16 18:13:58 -0500

    Adds editable function in post_serializer
    editable returns true if user_id matches the user_id in post

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,6 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :user_id
+  attributes :id, :title, :content, :user_id, :editable
+  def editable
+    scope == object.user
+  end
 end
